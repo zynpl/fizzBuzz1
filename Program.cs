23490d2c6e730c5c28768e279b838a545448c4dc
zeynep <81883067+zynpl@users.noreply.github.com>
2021-11-27 02:37:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace fizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Write a starting number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write ending number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=num1;i<=num2;i++)
+            {
+                if (i%3==0 && i%5==0)
+                {
+                    Console.WriteLine("FizzBuzz");
+
+                }
+                else if (i%3==0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i%5==0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            
+            
+            Console.Read();
+
+        }
+    }
+}
